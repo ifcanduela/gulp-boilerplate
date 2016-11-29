253b4d33624bef75124773296e31aac366b7c18a
Igor F. Canduela <ifcanduela@gmail.com>
2016-11-29 15:38:33 +0100

    Several improvements
    Configuration settings, notification toasts and require() modules via Browserify bundling

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,76 +1,198 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var watch = require("gulp-watch");
-var plumber = require("gulp-plumber");
-var batch = require("gulp-batch");
-var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
-var babel = require('gulp-babel');
-var rename = require('gulp-rename');
+// Select the CSS preprocessor to use
+// Available options are 'less', 'sass'; other values
+// will disable CSS preprocessing
+const CSS_PREPROCESSOR = 'less';
 
-var plumberConfig = {
+// Use Browserify to bundle JavaScript files
+const COMPILE_JS = true;
+
+const JS_WATCH_GLOB = './js/src/**/*.js';
+const MAIN_JS_FILE = './js/src/app.js';
+const JS_OUTPUT_PATH = './js/';
+const JS_OUTPUT_FILENAME = 'app.bundle.js';
+
+const LESS_WATCH_GLOB = './less/**/*.less';
+const SASS_WATCH_GLOB = './scss/**/*.scss';
+
+const MAIN_LESS_FILE = './less/main.less';
+const MAIN_SASS_FILE = './scss/main.scss';
+
+const CSS_OUTPUT_PATH = './css/';
+const CSS_OUTPUT_FILENAME = 'styles.css';
+
+// Choose whether to display a toast on error or not
+const USE_NOTIFIER_TOASTS = true;
+
+// Log errors to the console.
+// If the toasts are enabled the gulp-notify plugin will automatically log
+// to the console, so this is better disabled.
+const LOG_ERRORS_TO_CONSOLE = !USE_NOTIFIER_TOASTS;
+
+// By default no minification is enabled. Using the `gulp prod` task enables
+// minification of CSS and JS and disabled the sourcemaps.
+let PRODUCTION = false;
+
+let autoprefixer = require('autoprefixer');
+let babel = require('gulp-babel');
+let batch = require('gulp-batch');
+let browserify = require('browserify');
+let buffer = require('vinyl-buffer');
+let gulp = require('gulp');
+let gulpif = require('gulp-if');
+let less = require('gulp-less');
+let minifycss = require('gulp-minify-css');
+let notify = require('gulp-notify');
+let plumber = require('gulp-plumber');
+let postcss = require('gulp-postcss');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+let source = require('vinyl-source-stream');
+let sourcemaps = require('gulp-sourcemaps');
+let uglify = require('gulp-uglify');
+let watch = require('gulp-watch');
+
+let plumberConfig = {
     errorHandler: function (err) {
-        console.log(err.toString());
+        if (LOG_ERRORS_TO_CONSOLE) {
+            console.log(err.toString());
+        }
+
+        if (USE_NOTIFIER_TOASTS) {
+            notify.onError({
+                title: 'Gulp',
+                subtitle: 'Task error',
+                message: "<%= error.annotated ? error.annotated : error.message %>",
+                sound: 'Beep'
+            }) (err);
+        }
+
         this.emit('end');
     }
 };
 
-var postCssConfig = [
+let postCssConfig = [
     autoprefixer({
         browsers: ['last 2 versions']
     })
 ];
 
-var babelConfig = {
+let babelConfig = {
     presets: [
-        "es2015"
+        'es2015'
     ]
 };
 
-var renameCallback = function (path) {
-    path.basename = path.basename.replace('.babel', '');
-    return path;
-};
+//
+// Run `gulp less` to compile the LessCSS files.
+//
+gulp.task('less', () => {
+    return gulp.src(MAIN_LESS_FILE)
+        .pipe(plumber(plumberConfig))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(less())
+        .pipe(postcss(postCssConfig))
+        .pipe(gulpif(PRODUCTION, minifycss()))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(rename(CSS_OUTPUT_FILENAME))
+        .pipe(gulp.dest(CSS_OUTPUT_PATH));
+});
 
 //
 // Run `gulp sass` to compile the SCSS files.
 //
-gulp.task("sass", function () {
-    return gulp.src("./sass/styles.scss")
+gulp.task('sass', () => {
+    return gulp.src(MAIN_SASS_FILE)
         .pipe(plumber(plumberConfig))
-        .pipe(sourcemaps.init())
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass())
         .pipe(postcss(postCssConfig))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("./css/"));
+        .pipe(gulpif(PRODUCTION, minifycss()))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(rename(CSS_OUTPUT_FILENAME))
+        .pipe(gulp.dest(CSS_OUTPUT_PATH));
 });
 
 //
-// Run `gulp babel` to transpile EcmaScript 6 (2015) files in /js/src
+// Run `gulp css` to compile the stylesheets using the configured preprocessor.
 //
-gulp.task("babel", function () {
-    return gulp.src("./js/src/**/app.babel.js")
+gulp.task('css', () => {
+    if (CSS_PREPROCESSOR === 'less') {
+        gulp.start('less');
+    }
+
+    if (CSS_PREPROCESSOR === 'sass') {
+        gulp.start('sass');
+    }
+});
+
+//
+// Run `gulp js` to bundle the JavaScript files using Browserify and transpile
+// them using Babel
+//
+gulp.task('js', () => {
+    let b = browserify({
+        entries: MAIN_JS_FILE,
+        debug: true
+    });
+
+    return b.bundle()
+        .on('error', plumberConfig.errorHandler)
         .pipe(plumber(plumberConfig))
+        .pipe(source(JS_OUTPUT_FILENAME))
+        .pipe(buffer())
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init({loadMaps: true})))
         .pipe(babel(babelConfig))
-        .pipe(rename(renameCallback))
-        .pipe(gulp.dest("./js/"));
+        .pipe(gulpif(PRODUCTION, uglify()))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(JS_OUTPUT_PATH));
 });
 
 //
-// Run `gulp watch` to automatically compile the SCSS
+// Run `gulp watch` to automatically compile the LESS, SASS and JS
 // files when one of them is modified.
-// This command does not catch changes in files created
-// after the command is issued.
 //
-gulp.task("watch", function () {
-    watch("./**/*.scss", batch(function (events, done) {
-        gulp.start("sass", done);
-    }));
+gulp.task('watch', () => {
+    if (CSS_PREPROCESSOR === 'sass') {
+        watch(SASS_WATCH_GLOB, batch((events, done) => {
+            gulp.start('sass', done);
+        }));
+    }
 
-    watch("./js/src/**/*.js", batch(function (events, done) {
-        gulp.start("babel", done);
-    }));
+    if (CSS_PREPROCESSOR === 'less') {
+        watch(LESS_WATCH_GLOB, batch((events, done) => {
+            gulp.start('less', done);
+        }));
+    }
+
+    if (COMPILE_JS) {
+        watch(JS_WATCH_GLOB, batch((events, done) => {
+            gulp.start('js', done);
+        }));
+    }
 });
 
-gulp.task('default', ['sass', 'babel', 'watch']);
+//
+// Run `gulp watch` to automatically compile the LESS, SASS and JS
+// files when one of them is modified.
+//
+gulp.task('prod', () => {
+    PRODUCTION = true;
+
+    if (CSS_PREPROCESSOR === 'sass') {
+        gulp.start('sass');
+    }
+
+    if (CSS_PREPROCESSOR === 'less') {
+        gulp.start('less');
+    }
+
+    if (COMPILE_JS) {
+        gulp.start('js');
+    }
+});
+
+gulp.task('default', [
+    'css',
+    'js',
+    'watch'
+]);
--- README.md
+++ README.md
@@ -8,14 +8,13 @@ Clone the repo, `cd` into it and run `npm install`. Make sure you also have Gulp
 
 ## And then what sh-?
 
-You put your ES2015 files in the `js/src` folder with a `.babel.js` extension, and your SASS files in `sass`, with a `.scss` extension. The run
+You put your ES2015 files in the `js/src` folder with a `.js` extension, your SASS files in `scss` and LESS files in `less`, with `.scss` or `.less` extensions, respectively. Then run `gulp`.
 
 ## But does it co-?
 
-The Gulpfile includes commands to compile the stylesheets (`gulp sass`) and the
-scripts (`gulp babel`) separately, and the `gulp watch` command (or just `gulp`) 
-will run those two commands whenever a `.babel.js` or `.scss` file changes within
-the `js/src` and `sass` folders, respectively.
+The Gulpfile includes commands to compile the stylesheets (`gulp css`) and the
+scripts (`gulp js`) separately, and the `gulp watch` command (or just `gulp`)
+will run those two commands whenever a source file changes within the `js/src` and  `css` folders, respectively.
 
 ## Can I change th-?
 
